3e320104ad387f31e69163236a084a139a84054c
myGitUser13 <vanbeethoven01@yahoo.com>
2019-03-14 00:26:05 +0300

    Definitions is a doc helper, documenting some types

---
+++ app/Definitions.js
@@ -0,0 +1,17 @@
+/**
+ * @namespace Types
+ */
+
+/**
+ * @typedef {function} Types~service - A service responds to api requests.
+ * @param {object} req
+ * @param {object} res
+ * @param {object} next
+ */
+
+ /**
+ * @typedef {function} Types~middleware - An express middleware.
+ * @param {object} req
+ * @param {object} res
+ * @param {object} next
+ */
