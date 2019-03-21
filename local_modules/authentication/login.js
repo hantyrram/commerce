3e4e1c6b16cf7027fdd5b2aa52021a6fafb7d4aa
myGitUser13 <vanbeethoven01@yahoo.com>
2019-03-21 22:48:20 +0300

    Added projection on the findOne query so that it won't return the password

--- local_modules/authentication/login.js
+++ local_modules/authentication/login.js
@@ -9,9 +9,11 @@ const ObjectID = require('mongodb').ObjectID;
 module.exports = login = async(req,res)=>{
   console.log({body:req.body});
    try {     
-    let user = 
-     await req.app.get('db').collection('users').findOne({username:req.body.username,password:req.body.password});
+    let query = { username:req.body.username,password:req.body.password };
+    let project = { _id:1, username:1, roles:1 };
+    let user = await req.app.get('db').collection('users').findOne(query,project);
     if(user){
+      delete [user.password]; //just to be sure      
       req.login(null,user);
     }else{
       req.login('Invalid Username or Password',null);
