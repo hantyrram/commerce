76d0a64e2c4bc02b5942b5bd11a33af7e2fef807
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-01 00:57:58 +0300

    Removed code that checks the route and made it a helper function getRoute

--- app/middlewares/attachCurrentServiceToReq.js
+++ app/middlewares/attachCurrentServiceToReq.js
@@ -1,27 +1,24 @@
-/**
- * 
-* Attaches the current service being accessed by the request. This middleware should never fail,
-* to attach a service. Should be used before authorization. Uses the route definitions
-*/
 
 const config = require('../config');
 const pathToRegexp = require('path-to-regexp');
+const Artifact = require('../Artifact');
+const {getRoute} = require('../helpers');
 /**
  *
  * @type {HT~middleware}
- * @desc Attaches the @see{@link{Artifact}} to the response object. So that can be easily accessed by any service.
+ * @desc Attaches the @see{@link{HT~service}} and the @see {Types~route} that's being accessed, to the request object. 
+ * Authorization module and validateSchema middleware depends on this. 
+ * The service will ba attached as Request.currentAccessedService property.
+ * The route will be attached as Request.currentRoute property.
  */
 module.exports = attachCurrentServiceToReq = (req,res,next)=>{
-  let regexp;
-  let route = config.routes.find(route=>{
-     regexp = pathToRegexp(route.path);
-     return route.method.toLowerCase() === req.method.toLowerCase() && regexp.exec(req.path) !== null;
-  });
+  let route = getRoute(config.routes,req);
   //attache the service 
   if(!route){
-    next({status:'nok',type:'NOT_FOUND',message:'Can\'t find resource'});
+    next({status:'nok',source:'M_attach_current_service_to_req', type:'NOT_FOUND',message:'Can\'t find resource'});
     return;
   }
+  req.currentRoute = route;
   req.currentAccessedService = getServices().find(s=>{
     return s.name === route.serviceProvider;
   })
