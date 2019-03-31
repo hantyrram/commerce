ca56ec9cad89269f6d7e5b36c8439f216aec47db
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-01 00:52:06 +0300

    registered validateSchema route middleware

--- app/config/middlewares.js
+++ app/config/middlewares.js
@@ -1,7 +1,12 @@
+/**
+ * @namespace Config.middlewares
+ * @desc Middlewares MUST be registered here before use. Adding middleware here does not signify it's use.
+ * 
+ */
 module.exports = [
   '/app/middlewares/logger',
   '/app/middlewares/handleNonXHR',
   '/app/middlewares/attachCurrentServiceToReq',
   '/app/middlewares/attachArtifactToResponse',
-
+  '/app/middlewares/validateSchema'
 ]
