f714997c2d79a4e4f7eed6f3a179c9b78787eb78
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-01 01:00:44 +0300

    Added helpers folder, getRoute helper function

---
+++ app/helpers/index.js
@@ -0,0 +1,15 @@
+const pathToRegexp = require('path-to-regexp');
+/**
+ * Checks if there is a route definition that could handle the current req.method and req.path. This is 
+ * useful on middlewares defined before the route handlers.
+ * @param {Array} routes - Array of routes.
+ * @param {object} req - The request object.
+ */
+module.exports.getRoute = (routes,req) => {
+ let regexp;
+ let route = routes.find(route=>{
+  regexp = pathToRegexp(route.path);
+  return route.method.toLowerCase() === req.method.toLowerCase() && regexp.exec(req.path) !== null;
+ });
+ return route;
+}
