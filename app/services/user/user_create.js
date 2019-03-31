31b4d1ff59c35a76cb919ed5c6a78f0f2258b2f5
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-01 00:59:02 +0300

    Used Artifact.Message instead of Artifact.ArtifactMessage

--- app/services/user/user_create.js
+++ app/services/user/user_create.js
@@ -1,3 +1,4 @@
+
 /**
  * @type {HT~service}
  * @func user_create
@@ -9,7 +10,7 @@ module.exports = user_create = async (req,res)=>{
  //validate user;
  try {
   let result = await req.app.get('db').collection('users').insertOne(user);
-  let message = new Artifact.ArtifactMessage(Artifact.ArtifactMessage.SUCCESS,'User Created Successfully!');
+  let message = new Artifact.Message(Artifact.Message.SUCCESS,'User Created Successfully!');
   let data = {entity: {_id: result.insertedId, username: user.username}};
   let artifact = new Artifact('ok','user_create',data,message);
   res.json(artifact);
