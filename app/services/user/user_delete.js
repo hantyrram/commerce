e1565400d6b366c5777b6bb08db1b3cc0d5ef266
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-01 00:59:19 +0300

    Used Artifact.Message instead of Artifact.ArtifactMessage

--- app/services/user/user_delete.js
+++ app/services/user/user_delete.js
@@ -8,9 +8,9 @@ const ObjectID = require('mongodb').ObjectID;
 module.exports = user_delete = async (req,res,next)=>{
  //validate user;
  try {
-  let deleteWriteOpResult = await req.app.get('db').collection('users').deleteOne({username : req.params.username});
+  let deleteWriteOpResult = await req.app.get('db').collection('users').deleteOne({_id : req.params.id});
   console.log(deleteWriteOpResult.result);
-  let message = new Artifact.ArtifactMessage(Artifact.ArtifactMessage.SUCCESS,`${req.params.username} deleted!`);
+  let message = new Artifact.Message(Artifact.Message.SUCCESS,`${req.params.username} deleted!`);
    let artifact = new Artifact('ok','user_delete',null,message);
    res.json(artifact);
  } catch (error) {
