5bb1d5acf5766abbfe40eade75657c34fcb9a1b3
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-03 00:57:13 +0300

    Fixed error

--- app/error_handlers/5xx.js
+++ app/error_handlers/5xx.js
@@ -1,11 +1,12 @@
 const {ERRORS} = require('../config');
 module.exports = (artifact,req,res,next)=>{
+ console.log(artifact);
  if(artifact.error && artifact.error.type){
   switch(artifact.error.type){
     case ERRORS.SERVER_ERROR_SCHEMA_NOT_FOUND_ON_ROUTE :res.status(500).json(artifact);break;
     default:res.status(500).json(artifact);
   }
   return;
-}
-  res.status(500).json(err);
+ }
+  res.status(500).json(artifact);
 }
