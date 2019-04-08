46c23623a5658c6d8d1ddacf9f3ecd2ff34d37e0
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-09 01:09:25 +0300

    Added file,not yet implemented

--- app/services/user/user_roles_add.js
+++ app/services/user/user_roles_add.js
@@ -14,7 +14,4 @@ module.exports = user_roles_add = async (req,res,next)=>{
  module.exports.label = 'Add Role To User';
  
 
- // >> i click on user e.g. Ronskie
- // >> i click on add role > 
- // >> display me a list of roles > get_roles [return God's role]
- // updating a role that are embedded > check it on God > if it exists then update God > others
+ 
