b70b6e272370d6a075a5d6e693d52184ba693f66
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-13 00:42:27 +0300

    Added employee_credential_generate service provider

--- app/services/employee/employee_credential_generate.js
+++ app/services/employee/employee_credential_generate.js
@@ -2,31 +2,62 @@ const USERNAME_PREFIX = "htu"; //short for hantyr user
 const GENESIS = 0;
 const START = 1;
 
+
 /**
  * @type {HT~service}
  * @func employee_credential_generate
  * @memberof Services
- * @desc Creates a new user credential for the employee.
+ * @desc Generate and assigns the credential to the employee.
  */
 module.exports = employee_credential_generate = async (req,res,next)=>{
+//requires empID from param
+ let employees = req.app.get('db').collection('employees');
+ let { empID } = req.params; 
+ //return credential if employee has existing credential
+ let employeeWithCredential = await employees.findOne({ empID: empID, credential: { $exists:true }});
+ if(employeeWithCredential){
+  let error = new Artifact.Error('INVALID_OPERATION','Employee has existing Credential!');
+  let errorArtifact = new Artifact('nok', 'employee_credential_generate', error);
+  res.status(400).json(errorArtifact);
+  return;
+ }
+ //Determine the last used username
  const QUERY = { credential: { $exists: true } };
  const OPTIONS = { projection: { "credential.username": 1 } };
  const SORT = { "credential.username": -1 } 
  let username;
- let cursor = await req.app.get('db').collection('employees').find(QUERY,OPTIONS).sort(SORT).limit(1);
+ let cursor = await employees.find(QUERY,OPTIONS).sort(SORT).limit(1);
  if(! await cursor.hasNext()){
-  //genesis
+  //create first username
   username = `${USERNAME_PREFIX}${String(START).padStart(5,"0")}`;
  }else{
   let employee = await cursor.next();
-  username = `${USERNAME_PREFIX}${Number(employee.credential.username.replace(USERNAME_PREFIX,"")) + 1}`;
+  lastEmployeeNumber = Number(employee.credential.username.replace(USERNAME_PREFIX,""));
+  username = `${USERNAME_PREFIX}${String(lastEmployeeNumber + 1).padStart(5,"0")}`;
+ }
+ //--
+
+ //generate temporary password
+ let password = randomStrGenerator(10);
+ 
+ let credential = { username, password, temp: true ,createdOn: Date.now()};
+
+ const UPDATE = { 
+  $set: { credential },
+ };
+
+ let {matchedCount,modifiedCount,result,message} = await  employees.updateOne({ empID: empID },UPDATE);
+ if (matchedCount === 0){//query failed, empID does not exist
+  let error = new Artifact.Error('NOT_FOUND','Invalid Employee ID!');
+  let errorArtifact = new Artifact('nok', 'employee_credential_generate', error);
+  res.status(400).json(errorArtifact);
+  return;
  }
 
- let temporaryPassword = randomStrGenerator(10);
- res.json({
-  username: username,
-  password: temporaryPassword
- });
+ let msg  = new Artifact.Message(Artifact.Message.SUCCESS,`Username & Password created for EmpID: ${empID}!`);
+ let data = { entity: credential };
+ let artifact = new Artifact('ok', 'employee_credential_generate', data, msg);
+ res.status(201).json(artifact);
 }
 
-module.exports.label = 'Generate Employee Credential';
+module.exports.label = 'Generates a credential for an employee';
