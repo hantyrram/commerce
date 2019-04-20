27543b2b560ad4a085bdf63e835099a1bad8483c
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-20 18:18:01 +0300

    Removed redundant authentication services

--- app/services/authentication/authenticate.js
+++
@@ -1,8 +0,0 @@
-/**
- *@type {HT~service}
- *@module  Authentication/authenticate
- *@desc Authenticates the user.
- */
-module.exports = authenticate = (req,res,next)=>{
-  res.status(200).json({status:'ok',source:'authenticate',message:'Authentication Success!',data:{user:req.user}});
-}
