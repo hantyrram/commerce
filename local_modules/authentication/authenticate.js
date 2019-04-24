25b62e3b13c59958101e8cb4cb1e3a9636abd783
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-24 23:58:22 +0300

    Change user on artifact data to entity

--- local_modules/authentication/authenticate.js
+++ local_modules/authentication/authenticate.js
@@ -3,7 +3,13 @@
  *@desc Authenticates the user.
  */
 module.exports = authenticate = (req,res,next)=>{
-  res.status(200).json({status:'ok',source:'authenticate',message:'Authentication Success!',data:{user:req.user}});
+  const data = {
+   entity : {
+    ...req.user,
+    type: "user"
+   }
+  }
+  res.status(200).json({status:'ok',source:'authenticate',message:'Authentication Success!',data});
 }
 
 module.exports.permissionIsRequired = false;
