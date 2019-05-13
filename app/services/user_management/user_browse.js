b77608b4f9bcf752a992d9ebd80645d21a12f3fa
myGitUser13 <vanbeethoven01@yahoo.com>
2019-05-13 21:47:50 +0300

    Added User Browse

--- app/services/user_management/user_browse.js
+++ app/services/user_management/user_browse.js
@@ -4,8 +4,14 @@ module.exports = user_browse = async (req,res)=>{
   const INCLUDE_WITH_CREDENTIALS = {$project:{"credential":1}};
   const NO_PASSWORD = {$project:{"credential.password":0}};
   
-  let users = await req.app.get('db').collection('employees').aggregate([MATCH,INCLUDE_WITH_CREDENTIALS,NO_PASSWORD]).toArray();  
+  let employees = await req.app.get('db').collection('employees').aggregate([MATCH,INCLUDE_WITH_CREDENTIALS,NO_PASSWORD]).toArray();  
   let message = new Artifact.Message(Artifact.Message.INFO, 'Users');
+
+  let users = [];
+  for(let e of employees){
+   users.push({ _id: e._id, ...e.credential });
+  }
+
   let artifact = new Artifact('ok', 'user_browse', { entity: users } , message);
   res.json(artifact);
  } catch (error) {
