53497c011379b54a0f56c82ad302a791ca2369e7
hantyrram <hantyr@Hantyr.localdomain>
2019-11-13 13:13:04 +0300

    Added productCategory list api

---
+++ app/apis/productCategory_list.js
@@ -0,0 +1,8 @@
+module.exports = {
+   path : 'catalog/productcategories',
+   method: 'get',
+   resource: 'ProductCategory',
+   op: 'list',
+   serviceProvider: 'app/services/catalog/productcategory/list',
+   description: 'Retrieves Product Category List',
+}
