da63711224bb68790f2642de78a92365e054b5b9
hantyrram <hantyr@Hantyr.localdomain>
2020-05-19 20:27:28 +0300

    Added process.env log

--- app/index.js
+++ app/index.js
@@ -14,6 +14,7 @@ const dependencyManager = require('./dependencyManager');
 const server = express();
 const app = express();
 const PORT = process.env.PORT || 8080;
+console.log(process.env);
 
 const __defineGlobals = require('./__defineGlobals');
 __defineGlobals();
--- index.js
+++ index.js
@@ -4,4 +4,6 @@ global.SERVER_ROOT = __dirname;
 const path = require('path');
 //specify path, does not work in aws ubuntu if not specified
 require('dotenv').config({path: path.join(__dirname,'.env')}); 
+console.log('@Root Index: process.env', process.env);
 require('./app/index.js');
+
