b68f267b253f0927362373547b9ac6bcdb17da45
hantyrram <hantyr@Hantyr.localdomain>
2020-05-19 20:36:05 +0300

    Modified dotenv

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ global.SERVER_ROOT = __dirname;
 
 const path = require('path');
 //specify path, does not work in aws ubuntu if not specified
-require('dotenv').config({path: path.join(__dirname,'.env')}); 
+console.log(path.join(__dirname,'.env'));
+
+const dotenv = require('dotenv');
+
+dotenv.config({path: path.join(__dirname,'.env')});
+
 console.log('@Root Index: process.env', process.env);
+
 require('./app/index.js');
 
