d6685ec4422a247acc95410d44af42594ca83999
hantyrram <hantyr@Hantyr.localdomain>
2020-05-19 19:25:15 +0300

    Modified Api List temp

--- app/services/Api_List.js
+++ app/services/Api_List.js
@@ -10,14 +10,14 @@
  */
 module.exports = async (req,res,next)=>{ 
    
-   let apis = req.app.get('apis');
+   // let apis = req.app.get('apis');
 
-   let modifiedApis = apis.map(api=>{
-      api.serviceProvider = api.serviceProvider.replace(/\//g,'.');
-      return api;
-   });
-   console.log(modifiedApis);
-   res.json({ok: 1, resource: modifiedApis, resourceType:'Array', resourceArrayItemType: 'Role'});
+   // let modifiedApis = apis.map(api=>{
+   //    api.serviceProvider = api.serviceProvider.replace(/\//g,'.');
+   //    return api;
+   // });
+   // console.log(modifiedApis);
+   res.json({ok: 1, resource: [], resourceType:'Array', resourceArrayItemType: 'Api'});
 }
 
 module.exports.api = {
